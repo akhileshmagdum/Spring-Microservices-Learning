6dcfbd0aec141b277c7f79074455f59d813738cb
akhileshmagdum <akhileshmagdum@gmail.com>
2022-09-13 21:05:34 +0530

    Eureka client created

--- user-app/src/main/java/com/akhilesh/userapp/UserAppApplication.java
+++ user-app/src/main/java/com/akhilesh/userapp/UserAppApplication.java
@@ -2,7 +2,9 @@ package com.akhilesh.userapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserAppApplication {
 
