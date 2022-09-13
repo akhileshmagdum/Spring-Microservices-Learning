d7814d6434eb20cb518fd61153a64784ebeff996
akhileshmagdum <akhileshmagdum@gmail.com>
2022-09-13 15:28:41 +0530

    Eureka server project setup

---
+++ discovery-service/src/test/java/com/akhilesh/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhilesh.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
