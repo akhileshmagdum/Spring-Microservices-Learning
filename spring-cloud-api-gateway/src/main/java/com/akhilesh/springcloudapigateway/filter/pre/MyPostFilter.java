dc4cf7fc422e0b8c3e4613d17b265308d45960bb
akhileshmagdum <akhileshmagdum@gmail.com>
2022-09-22 16:30:27 +0530

    Global Post-Filter

---
+++ spring-cloud-api-gateway/src/main/java/com/akhilesh/springcloudapigateway/filter/pre/MyPostFilter.java
@@ -0,0 +1,18 @@
+package com.akhilesh.springcloudapigateway.filter.pre;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyPostFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            System.out.println("\n\nThis is a post filter\n\n");
+        }));
+    }
+}
