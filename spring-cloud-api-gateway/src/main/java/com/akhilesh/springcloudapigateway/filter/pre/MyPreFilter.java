2e08e911b5849968f31ac427ff9d90b98137b930
akhileshmagdum <akhileshmagdum@gmail.com>
2022-09-22 16:24:42 +0530

    Global Pre-Filter

---
+++ spring-cloud-api-gateway/src/main/java/com/akhilesh/springcloudapigateway/filter/pre/MyPreFilter.java
@@ -0,0 +1,19 @@
+package com.akhilesh.springcloudapigateway.filter.pre;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Pre-filter - Runs before the request is sent to the microservice
+ */
+@Component
+public class MyPreFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("\n\nThis is a filter\n" + exchange.getRequest().getPath().toString() + "\n");
+        return chain.filter(exchange);
+    }
+}
