9917a631048c1a07f099541f73e6e04f78f0dd7d
akhileshmagdum <akhileshmagdum@gmail.com>
2022-09-22 15:24:51 +0530

    Custom filter with AbstractGatewayFilterFactory

---
+++ spring-cloud-api-gateway/src/main/java/com/akhilesh/springcloudapigateway/config/AuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.akhilesh.springcloudapigateway.config;
+
+import io.jsonwebtoken.Jwts;
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthorizationFilter extends AbstractGatewayFilterFactory<AuthorizationFilter.MyConfig> {
+
+    /**
+     * All the configurable properties go here
+     */
+    public static class MyConfig {
+
+    }
+
+    @Override
+    public GatewayFilter apply(MyConfig config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return throwError(exchange, HttpStatus.BAD_REQUEST);
+            }
+            ;
+
+            if (!checkJwtValid(request.getHeaders()
+                    .get(HttpHeaders.AUTHORIZATION)
+                    .get(0).replace("Bearer ", ""))) {
+                return throwError(exchange, HttpStatus.BAD_GATEWAY);
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> throwError(ServerWebExchange webExchange, HttpStatus status) {
+        ServerHttpResponse response = webExchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
+    }
+
+    private boolean checkJwtValid(String jwt) {
+        String subject = Jwts.parser()
+                .setSigningKey("somethingforjwttokentosignthistokenwith")
+                .parseClaimsJws(jwt)
+                .getBody().getSubject();
+        if (subject.isEmpty() || subject == null) {
+            return false;
+        }
+        return true;
+    }
+}
--- spring-cloud-api-gateway/src/main/java/com/akhilesh/springcloudapigateway/config/CustomRoutes.java
+++ spring-cloud-api-gateway/src/main/java/com/akhilesh/springcloudapigateway/config/CustomRoutes.java
@@ -1,24 +1,34 @@
 package com.akhilesh.springcloudapigateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 
 @Configuration
 public class CustomRoutes {
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes().route("user-service", route -> route
-                        .path("/**")
-                        /*
-                         * Replaces the url matched with the regex with the replacement string
-                         */
-                        .filters(gatewayFilterSpec ->
-                                gatewayFilterSpec.rewritePath("/regex","/something"))
-                        .uri("lb://user-app"))
+        return builder.routes()
+                .route("user-service",
+                        route -> route
+                                .path("/something")
+                                .and()
+                                /*
+                                 *Checking bearer token in header
+                                 */
+                                .header("Authorization","Bearer (.*)")
+                                .filters(f-> f.filter(authorizationFilter.apply(new AuthorizationFilter.MyConfig())))
+                                /*
+                                 * Replaces the url matched with the regex with the replacement string
+                                 */
+//                                .filters(gatewayFilterSpec ->
+//                                        gatewayFilterSpec.rewritePath("/regex", "/something"))
+                                .uri("lb://user-app"))
                 .build();
     }
+    @Autowired
+    private AuthorizationFilter authorizationFilter;
 }
