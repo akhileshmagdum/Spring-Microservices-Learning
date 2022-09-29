cd6cfb84896e9a2471f97e1262b6bffeebd72653
akhileshmagdum <akhileshmagdum@gmail.com>
2022-09-29 20:26:00 +0530

    Http Request logging in Feign client

--- user-app/src/main/java/com/akhilesh/userapp/UserAppApplication.java
+++ user-app/src/main/java/com/akhilesh/userapp/UserAppApplication.java
@@ -1,5 +1,6 @@
 package com.akhilesh.userapp;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -27,5 +28,10 @@ public class UserAppApplication {
 	@LoadBalanced //To enable client side load balancing
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 }
